05d06055c73fbe2c685789c628f03c578c350e82
Brett Farewell <brettrfarewell@gmail.com>
2025-10-05 19:06:48 -0700

    Updated test_encoder with more test cases

--- src/test_encoder.cpp
+++ src/test_encoder.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <string>
+#include <iostream>
 #include "huffman.hpp"
 
 using namespace std;
@@ -10,14 +11,26 @@ int main() {
     huffman::Decoder decoder;
 
     const vector<uint8_t> input = {'A', 'A', 'A', 'B', 'B', 'C'};
-
     const vector<uint8_t> output = encoder.encode(input);
-
     assert(!output.empty());
-
     const vector<uint8_t> decode_out = decoder.decode(output);
 
+    const vector<uint8_t> input_a = {'A', 'A', 'A'};
+    const vector<uint8_t> output_a = encoder.encode(input);
+    assert(!output_a.empty());
+    const vector<uint8_t> decode_out_a = decoder.decode(output);
+
+    vector<uint8_t> input_stress;
+    for (uint16_t i = 0; i < 256; ++i)
+        for (int j = 0; j < 1024; ++j)
+            input_stress.push_back((uint8_t) i);
+    const vector<uint8_t> output_stress = encoder.encode(input);
+    assert(!output_stress.empty());
+    const vector<uint8_t> decode_out_stress = decoder.decode(output);
+
     assert(decoder.decode(output) == input);
+
+    cout << "ALL TESTS PASS" << "\n";
 
     return 0;
 }
